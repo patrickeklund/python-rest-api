ebc62b50caa3121274d7997146656ebd5babf2ef
Patrick Eklund <patrickeklund@users.noreply.github.com>
2025-04-06 12:10:54 +0200

    Add files via upload

--- model/restModel.py
+++ model/restModel.py
@@ -1,1 +1,13 @@
+# Define Pydantic model for input data validation
 
+# Import
+from pydantic import BaseModel, Field, PositiveFloat
+
+
+# Basig model, change for each application
+class Item(BaseModel):
+	name: str
+	description: str | None = None
+	price: float 
+	price2: PositiveFloat
+	tax: float | None = None
